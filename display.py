bb1b011c6eee270d42b5abb79c5b8d484c1d1232
Sadam Hussai Alvi <sadamalvi79@gmail.com>
2026-01-31 22:38:56 +0500

    Resolve merge conflict in display.py

--- display.py
+++ display.py
@@ -1,6 +1,6 @@
 
 import streamlit as st
-<<<<<<< HEAD
+
 import pickle
 import pandas as pd
 
@@ -17,14 +17,6 @@ def recommend(title):
     return recommended
 
 
-
-
-
-=======
-from app import movie_recommend  
-import pandas as pd
-
->>>>>>> 1dc13e4356e874dcb0abf2eec991141eaf81c405
 st.markdown("<h1 style='color: darkblue;'>🎬 Movie Recommender</h1>", unsafe_allow_html=True)
 df = pd.read_csv("tmdb_5000_movies.csv")
 # Dropdown
@@ -33,11 +25,7 @@ selected_movie = st.selectbox("Select a Movie 🎬 ",df["original_title"].values
 
 # Button to recommend
 if st.button("Recommend"):
-<<<<<<< HEAD
-    recommendations = recommend(selected_movie)
-=======
-    recommendations = movie_recommend(selected_movie)
->>>>>>> 1dc13e4356e874dcb0abf2eec991141eaf81c405
     st.write("Top 5 recommended movies:")
+    recommendations = recommend(selected_movie)
     for i, movie in enumerate(recommendations, 1):
         st.write(f"{i}. {movie}")
